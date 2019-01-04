964de6c21faba02f4d9b9b5b5832dc5a0f43d9b5
disjfa <disjfa@disjfa.nl>
2019-01-04 22:52:14 +0000

    Updates

---
+++ precache-manifest.a063c769ae9180c7e6222c10e66eb1ae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c41b97b6b78c198062d0",
+    "url": "/js/chunk-vendors.6daf5a43.js"
+  },
+  {
+    "revision": "c55235ee822c8538846b",
+    "url": "/js/app.dac53b96.js"
+  },
+  {
+    "revision": "985aefa1037e0129487fb710feba049d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/img/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/fonts/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/fonts/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/fonts/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/fonts/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "c55235ee822c8538846b",
+    "url": "/css/app.c3822dc9.css"
+  }
+];
