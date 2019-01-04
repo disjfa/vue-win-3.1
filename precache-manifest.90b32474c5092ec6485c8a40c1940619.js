88b11feb0cf4157c55c4cfcb62322a15bdc109a1
disjfa <disjfa@disjfa.nl>
2019-01-04 22:56:49 +0000

    Updates

---
+++ precache-manifest.90b32474c5092ec6485c8a40c1940619.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-win-3.1/robots.txt"
+  },
+  {
+    "revision": "c41b97b6b78c198062d0",
+    "url": "/vue-win-3.1/js/chunk-vendors.6daf5a43.js"
+  },
+  {
+    "revision": "d98728e69c25d7ff8322",
+    "url": "/vue-win-3.1/js/app.820f5745.js"
+  },
+  {
+    "revision": "b45704b01870a2cfc99303c26bac36dd",
+    "url": "/vue-win-3.1/index.html"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/vue-win-3.1/img/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/vue-win-3.1/fonts/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/vue-win-3.1/fonts/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/vue-win-3.1/fonts/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/vue-win-3.1/fonts/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "d98728e69c25d7ff8322",
+    "url": "/vue-win-3.1/css/app.c3822dc9.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a063c769ae9180c7e6222c10e66eb1ae.js"
+  "/vue-win-3.1/precache-manifest.90b32474c5092ec6485c8a40c1940619.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "win3.1"});
